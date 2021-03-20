6cc2abfe5d992c61b6e9565e68bac0966595a82b
Cihat Solak <ExtCihatS@d-teknoloji.com.tr>
2021-03-20 22:54:09 +0300

    web project has been added.

--- NetCoreSignalR/SignalR.API/Startup.cs
+++ NetCoreSignalR/SignalR.API/Startup.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using SignalR.API.Hubs;
 
 namespace SignalR.API
