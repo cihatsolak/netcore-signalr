a48bda8a014ecec7cac905e48960f5384a2bbf62
Cihat Solak <ExtCihatS@d-teknoloji.com.tr>
2021-03-21 00:38:12 +0300

    AddRazorRuntimeCompilation

--- NetCoreSignalR/SignalR.Web/Controllers/HomeController.cs
+++ NetCoreSignalR/SignalR.Web/Controllers/HomeController.cs
@@ -4,8 +4,21 @@ namespace SignalR.Web.Controllers
 {
     public class HomeController : Controller
     {
-        //Temel Konuları Ele Aldık
+        /// <summary>
+        /// Temel konuları ele aldık. Hub'a baglantı kurduk ve mesaj gönderip mesaj aldık.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Basic()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// SignalR'da loglama seviyesine göre loglama yapısı oluşturduk.
+        /// ConnectionState özelliğini kullandık.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Log()
         {
             return View();
         }
