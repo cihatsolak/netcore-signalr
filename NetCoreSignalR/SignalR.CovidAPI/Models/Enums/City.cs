534b34691a962efa7c1d3e5931e5a112cbb1aa00
Cihat Solak <ExtCihatS@d-teknoloji.com.tr>
2021-03-24 21:44:49 +0300

    the project was built.

---
+++ NetCoreSignalR/SignalR.CovidAPI/Models/Enums/City.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.CovidAPI.Models.Enums
+{
+    public enum City
+    {
+        Istanbul = 1,
+        Ankara = 2,
+        Izmir = 3,
+        Konya = 4,
+        Antalya = 5
+    }
+}
---
+++ NetCoreSignalR/SignalR.CovidAPI/Startup.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SignalR.CovidAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.CovidAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
