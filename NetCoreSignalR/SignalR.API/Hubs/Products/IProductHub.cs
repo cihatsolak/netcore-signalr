85281da95e85ee9d5c92bbd27404ddd8f4ade42f
Cihat Solak <ExtCihatS@d-teknoloji.com.tr>
2021-03-24 21:31:05 +0300

    strongly typed hubs

---
+++ NetCoreSignalR/SignalR.API/Hubs/Products/IProductHub.cs
@@ -0,0 +1,13 @@
+﻿using SignalR.API.Models;
+using System.Threading.Tasks;
+
+namespace SignalR.API.Hubs.Products
+{
+    /// <summary>
+    /// Clientlarda çalışacak olan metotlarımızı yazıyoruz.
+    /// </summary>
+    public interface IProductHub //Örneğin productlarla çalışan realtime uygulamamız var.
+    {
+        Task ReceiveProduct(Product product);
+    }
+}
