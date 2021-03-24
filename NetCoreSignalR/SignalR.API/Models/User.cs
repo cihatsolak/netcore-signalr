c58ae6d965a3828fb6570574145a68df034a5d43
Cihat Solak <ExtCihatS@d-teknoloji.com.tr>
2021-03-24 12:02:26 +0300

    database connection has been made.

---
+++ NetCoreSignalR/SignalR.API/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SignalR.API.Models
+{
+    /// <summary>
+    /// Veri tabanı user tablomuz
+    /// </summary>
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual Team Team { get; set; }
+    }
+}
