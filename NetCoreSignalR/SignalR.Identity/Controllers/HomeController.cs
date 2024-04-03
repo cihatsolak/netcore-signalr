ea61f9cbfb02a625dccebf9643234463dbacddec
Cihat Solak <cihatsolak@hotmail.com>
2024-04-03 23:24:19 +0300

    add signalr with identity project

---
+++ NetCoreSignalR/SignalR.Identity/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using SignalR.Identity.Models;
+using System.Diagnostics;
+
+namespace SignalR.Identity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
