e12f8f28d76449e5259da08b8a807edbc05ed9b7
Cihat Solak <cihatsolak@hotmail.com>
2024-04-04 16:54:39 +0300

    create excel file via queues

---
+++ NetCoreSignalR/SignalR.Identity/BackgroundServices/CreateExcelBackgroundService.cs
@@ -0,0 +1,54 @@
+﻿namespace SignalR.Identity.BackgroundServices;
+
+public class CreateExcelBackgroundService(
+    Channel<(string userId, List<Product> products)> channel,
+    IFileProvider fileProvider,
+    IServiceProvider serviceProvider) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (await channel.Reader.WaitToReadAsync(stoppingToken))
+        {
+            await Task.Delay(4000, stoppingToken);
+
+            var (userId, products) = await channel.Reader.ReadAsync(stoppingToken);
+
+            var physicalPath = fileProvider.GetDirectoryContents("wwwroot").Single(x => x.Name == "files").PhysicalPath;
+
+            var newExcelFilePath = Path.Combine(physicalPath, $"product-list-{Guid.NewGuid()}.xlsx");
+
+            var dataSet = new DataSet();
+            dataSet.Tables.Add(GetTable("Product List", products));
+
+            var workBook = new XLWorkbook();
+            workBook.Worksheets.Add(dataSet);
+
+            await using var excelFileStream = new FileStream(newExcelFilePath, FileMode.Create);
+            workBook.SaveAs(excelFileStream);
+
+            //using var scope = serviceProvider.CreateScope();
+            //var appHub = scope.ServiceProvider.GetRequiredService<IHubContext<AppHub>>();
+
+
+            //await appHub.Clients.User(userId).SendAsync("AlertCompleteFile", $"/files/{newExcelFilePath}", stoppingToken);
+        }
+    }
+
+    private static DataTable GetTable(string tableName, List<Product> products)
+    {
+        var table = new DataTable(tableName);
+
+        foreach (var item in typeof(Product).GetProperties())
+        {
+            table.Columns.Add(item.Name, item.PropertyType);
+        }
+
+
+        products.ForEach(product =>
+        {
+            table.Rows.Add(product.Id, product.Name, product.Price, product.Description, product.UserId);
+        });
+
+        return table;
+    }
+}
--- NetCoreSignalR/SignalR.Identity/Program.cs
+++ NetCoreSignalR/SignalR.Identity/Program.cs
@@ -3,10 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AppDbContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("SignalRIdentityDb"));
-});
+builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Directory.GetCurrentDirectory()));
+builder.Services.AddScoped<FileService>();
 
 //Channel.CreateBounded() 
 //kuyrukta kaç mesaj bulunmasý gerektiðini belirtiyorsunuz.
@@ -14,14 +12,21 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 //Channel.CreateUnbounded() 
 //Kuyrukta kaç mesaj bulunmasý gerektiðini belirtmiyorsunuz. Alabildiði kadar mesajý kabul ediyor.
 
-var channel = Channel.CreateUnbounded<Tuple<string, List<Product>>>();
+var channel = Channel.CreateUnbounded<(string, List<Product>)>();
 builder.Services.AddSingleton(channel);
 
-builder.Services.AddScoped<FileService>();
 builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("SignalRIdentityDb"));
+
+});
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddHostedService<CreateExcelBackgroundService>();
 
 var app = builder.Build();
 
